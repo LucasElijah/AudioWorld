db2ee390c2421701b89f2f1728ff85f787a19130
LucasElijah <elijahlucas96@gmail.com>
2022-09-03 14:53:39 -0700

    l1

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="frame">
+			{/* side */}
+			<div className="sidebar">
+				{/* header */}
+				<div className="sidebar-header">
+          
+        </div>
+
+				{/* content */}
+				<div className="sidebar-content"></div>
+
+				{/* footer */}
+				<div className="sidebar-footer"></div>
+			</div>
+
+			{/* main */}
+			<div className="main">
+				{/* nav */}
+				<div className="main-nav"></div>
+
+				{/* body */}
+				<div className="main-content"></div>
+        
+			</div>
+		</div>
+	);
+}
+
+export default App;
